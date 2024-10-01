c6497ade86fabd16ee1e9d64db986928bdd977cc
Alexandre PICARD <picarda@TURGOT.LAN>
2024-10-01 17:58:05 +0200

    First Pignat

---
+++ First_Pignat/First_Pignat/Fonction.cpp
@@ -0,0 +1,25 @@
+// Exemple d'utilisation de la bibliothèque NI DAQ
+// BTS IRIS, Avril 2013
+#include <iostream> // pour cin, cout
+#include <Windows.h> // pour la fonction Sleep()
+// prototypes des fonctions DAQmx, ce fichier doit être placé dans le répertoire de votre projet
+#include "NIDAQmx.h"
+#include "EsMelangeur.h"
+#include "head.h"
+using namespace std;
+// Fonction principale
+int fonctiondep(void)
+{
+
+	cout << "..:: Debut programme ::.." << endl;
+
+	CEsMelangeur melangeur;
+	melangeur.initEsMelangeur();
+
+
+
+	melangeur.changerEtatSorties();
+	
+	cout << "..:: Fin programme ::.." << endl;
+	return 0;
+}
